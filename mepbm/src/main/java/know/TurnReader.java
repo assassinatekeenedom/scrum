992cb8a756551da3aeeff0fa7f2beb5fc8c62a5f
rnkeene <rnkeene@gmail.com>
2015-08-08 20:46:00 -0400

    An Automated Commit Message

--- mepbm/src/main/java/know/TurnReader.java
+++ mepbm/src/main/java/know/TurnReader.java
@@ -2,6 +2,7 @@ package know;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -18,6 +19,39 @@ public class TurnReader extends API {
 
     private static final String pdfA = "C:\\selenium\\game\\game143\\Turn00\\g143n05t000.pdf";
 
+    public static String read(String path) {
+        try {
+            PDDocument xpdf = PDDocument.loadNonSeq(new File(path), null);
+            PDFTextStripper extract = new PDFTextStripper();
+            extract.setStartPage(0);
+            extract.setEndPage(xpdf.getNumberOfPages());
+            return extract.getText(xpdf);
+        } catch (IOException ex) {
+        }
+        return "Failed to read PDF at path: " + path;
+    }
+
+    public static List<Character> toons(String pdf) throws Exception {
+        String ending = pdf.substring(pdf.indexOf("ORDERS GIVEN"), pdf.length());
+        String[] lines = ending.split("\n");
+        List toons = new ArrayList<Character>();
+        for (String line : lines) {
+            if (line.indexOf("Ranks") > 0) {
+                String name = line.substring(0, line.indexOf(" Ranks"));
+                String com = line.substring(line.indexOf("Command"), line.indexOf("Agent"));
+                String agent = line.substring(line.indexOf("Agent"), line.indexOf("Emissary"));
+                String emissary = line.substring(line.indexOf("Emissary"), line.indexOf("Mage"));
+                String mage = line.substring(line.indexOf("Mage"), line.length());
+                com = com.replaceAll("Command", "");
+                agent = agent.replaceAll("Agent", "");
+                emissary = emissary.replaceAll("Emissary", "");
+                mage = mage.replaceAll("Mage", "");
+                toons.add(new Character(name, Integer.parseInt(com.trim()), Integer.parseInt(agent.trim()), Integer.parseInt(emissary.trim()), Integer.parseInt(mage.trim())));
+            }
+        }
+        return toons;
+    }
+
     @Override
     public void run() {
         setProcess(getClass().getCanonicalName());
@@ -29,13 +63,13 @@ public class TurnReader extends API {
     }
 
     @GET
-    @Path("/test.js")
+    @Path("/{game}/{turn}/{nation}.js")
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public String write() throws Exception {
-        System.out.println("HELLO WORLD!");
-        NationTurn turn = NationTurn.create(pdfA);
+    public String write(@PathParam("game") String game, @PathParam("turn") String turn, @PathParam("nation") String file) throws Exception {
+        String path = "C:\\mepbm\\games\\fourthage\\" + game + "\\" + turn + "\\" + file + ".pdf";
+        System.out.println("HELLO WORLD ... path ...! " + path);
 //        System.out.println("Callback (JSONP)" + callback + " of Game #" + game + " for turn #" + turn+ " for nation #" + nation);
-        return  "jsonp(" + new CharacterSection(turn).call() + ");";
+        return "jsonp(" + TurnReader.toons(TurnReader.read(path)) + ");";
     }
 
     public TurnReader() {
--- mepbm/src/test/java/know/ReadPDF.java
+++ mepbm/src/test/java/know/ReadPDF.java
@@ -6,15 +6,15 @@ import org.testng.annotations.Test;
 public class ReadPDF {
 
     private static final String DESTINATION = "C:\\selenium\\mepbm\\www\\";
-    private static final String pdfA = "C:\\selenium\\game\\game143\\Turn00\\g143n05t000.pdf";
+    private static final String pdfA = "C:\\selenium\\game\\game143\\Turn00\\g143n04t000.pdf";
+//    private static final String pdfA = "C:\\selenium\\game\\game143\\Turn00\\g143n05t000.pdf";
 
     public ReadPDF() {
     }
 
     @Test
     public void testNationTurn() throws Exception {
-        NationTurn turn = NationTurn.create(pdfA);
-        List<Character> toons = new CharacterSection(turn).call();
+        List<Character> toons = TurnReader.toons(TurnReader.read(pdfA));
         System.out.println(toons);
     }
 }
